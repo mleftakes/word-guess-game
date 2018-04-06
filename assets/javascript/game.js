0629793677a1df9901308f1f64434b9a6d17b8bc
mleftakes <mleftakes@gmail.com>
2018-04-05 21:10:02 -0500

    thursday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-const word = ["Michael Jackson", "Madonna", "Prince", "U2", "Van Halen", "Bruce Springsteen"];
+var word = ["Michael Jackson", "Madonna", "Prince", "U2", "Van Halen", "Bruce Springsteen"];
 
 var randNum = Math.floor(Math.random() * word.length);
 var chosenWord = word[randNum];
@@ -17,12 +17,20 @@ function generateUnderScore () {
 console.log(generateUnderScore());
 
 
-document.addEventListener("keypress", (event) => {
-    var keycode = event.keyCode;
-    var keyword = String.fromCharCode(event.keycode);
+// document.addEventListener("keypress", (event) => {
+//     var keycode = event.keyCode;
+//     var keyword = String.fromCharCode(event.keycode);
+//     console.log(chosenWord.indexOf(keyword));
+//     if(keyword.indexOf(chosenWord) > -1) {
+//         console.log(true);
+//     }
+// });
+
+document.addEventListener("keypress", function (event) {
+    // var keycode = event.keycode;
+    var keyword = String.fromCharCode(event.keyCode);
     console.log(chosenWord.indexOf(keyword));
     if(keyword.indexOf(chosenWord) > -1) {
         console.log(true);
     }
 });
-
